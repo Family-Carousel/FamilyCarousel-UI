23eb612ce40450166fac8b160e2066494f58d5d3
Michael Cavanaugh <funkel1989@gmail.com>
2022-08-16 12:46:17 -0400

    adding in the mailchimp api key

--- src/server/start.js
+++ src/server/start.js
@@ -5,7 +5,6 @@ const createApp = require("./app");
 
 console.log("Server Starting");
 logger.log("Global Logger Enabled");
-logger.log(process.env);
 
 process.on("uncaughtException", (err) => {
   logger.error("uncaughtException", err);
