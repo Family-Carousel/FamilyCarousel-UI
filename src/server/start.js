645053751adc97ddeea7aee4212d3c0bcac6d15e
Michael Cavanaugh <funkel1989@gmail.com>
2022-08-16 11:21:10 -0400

    logging env vars

--- src/server/start.js
+++ src/server/start.js
@@ -2,9 +2,11 @@
 
 require("./utils/globalsAndEnv");
 const createApp = require("./app");
+const logger = require("./services/helpers/logger");
 
 console.log("server starting");
 logger.log("Global Logger Enabled");
+console.log(process.env);
 
 process.on("uncaughtException", (err) => {
   logger.error("uncaughtException", err);
