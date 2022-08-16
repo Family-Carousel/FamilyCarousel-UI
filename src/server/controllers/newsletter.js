11764c7a8dcf08d09bec97e79fcf7d8bf7586880
Michael Cavanaugh <funkel1989@gmail.com>
2022-08-16 14:54:47 -0400

    changing things up

--- src/server/controllers/newsletter.js
+++ src/server/controllers/newsletter.js
@@ -3,38 +3,34 @@
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 exports.subscribe = async (req, res, next) => {
-  try {
-    mailchimp.setConfig({
-      apiKey: process.env.MAILCHIMP_API_KEY,
-      server: "us14",
+  mailchimp.setConfig({
+    apiKey: process.env.MAILCHIMP_API_KEY,
+    server: "us14",
+  });
+
+  const { subscriptionInformation } = req.body;
+
+  await mailchimp.lists
+    .addListMember(subscriptionInformation.listId, {
+      email_address: subscriptionInformation.emailAddress,
+      status: "pending",
+    })
+    .then((r) => {
+      logger.log(
+        `Successfully added contact email ${subscriptionInformation.emailAddress} as an audience member to list with Id ${subscriptionInformation.listId}. The contact's id is ${r.id}.`
+      );
+
+      res.json({
+        status: "success",
+        message: `Successfully added email ${subscriptionInformation.emailAddress}`,
+      });
+    })
+    .catch((err) => {
+      logger.log(err);
+      res.json({
+        status: "error",
+        error: err,
+        message: `Failed to add email ${subscriptionInformation.emailAddress}`,
+      });
     });
-
-    const { subscriptionInformation } = req.body;
-
-    logger.log(subscriptionInformation);
-
-    const response = await mailchimp.lists.addListMember(
-      subscriptionInformation.listId,
-      {
-        email_address: subscriptionInformation.emailAddress,
-        status: "pending",
-      }
-    );
-
-    logger.log(
-      `Successfully added contact email ${subscriptionInformation.emailAddress} as an audience member to list with Id ${subscriptionInformation.listId}. The contact's id is ${response.id}.`
-    );
-
-    res.json({
-      status: "success",
-      message: `Successfully added email ${subscriptionInformation.emailAddress}`,
-    });
-  } catch (err) {
-    logger.log(err);
-    res.json({
-      status: "error",
-      error: err,
-      message: `Failed to add email ${subscriptionInformation.emailAddress}`,
-    });
-  }
 };
