712e56127e05a530e03180381df5749c45621986
Michael Cavanaugh <funkel1989@gmail.com>
2022-08-15 22:18:50 -0400

    trying something else

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   srcDir: "src/",
   telemetry: false,
+  server: {
+    host: "0.0.0.0",
+    port: "80",
+  },
+  dev: process.env.NODE_ENV !== "production",
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: "%s - Family Carousel",
     title: "Family Carousel",
-    server: {
-      host: '0.0.0.0'
-    },
-    dev: process.env.NODE_ENV !== 'production',
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
@@ -25,8 +26,8 @@ module.exports = {
         href: "https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;600;700;800;900&display=swap",
       },
       {
-        rel: '',
-        href: 'https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css'
+        rel: "",
+        href: "https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css",
       },
       // ...config.icons.map((href) => ({ rel: "stylesheet", href })),
     ],
@@ -35,11 +36,11 @@ module.exports = {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: ["~/assets/scss/theme.scss"],
 
-  loading: { 
+  loading: {
     color: "blue",
-    height: '5px',
+    height: "5px",
     continuous: true,
-    trottle: 75
+    trottle: 75,
   },
 
   vue: {
@@ -60,9 +61,7 @@ module.exports = {
     { src: "~/filters/formatDate.js" },
   ],
 
-  serverMiddleware: [
-    { path: '/api', handler: '~/server/start.js' }
-  ],
+  serverMiddleware: [{ path: "/api", handler: "~/server/start.js" }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   // components: true,
@@ -82,7 +81,7 @@ module.exports = {
       },
     ],
   ],
-  
+
   // render: {
   //   bundleRenderer: {
   //     runInNewContext: false
@@ -92,10 +91,10 @@ module.exports = {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     "@nuxtjs/google-gtag",
-    'cookie-universal-nuxt',
+    "cookie-universal-nuxt",
     // "@nuxtjs/proxy",
     "@nuxtjs/axios",
-    '@nuxtjs/auth-next',
+    "@nuxtjs/auth-next",
     "@nuxt/image",
     [
       "nuxt-validate",
@@ -107,17 +106,17 @@ module.exports = {
   ],
 
   "google-gtag": {
-    id: 'gaId',
+    id: "gaId",
     debug: true, // enable to track in dev mode
     disableAutoPageTrack: false, // disable if you don't want to track each page route with router.afterEach(...).
   },
 
   auth: {
     redirect: {
-      login: '/auth/loggedin',
-      callback: '/auth/loggedin',
-      home: '/',
-      logout: '/'
+      login: "/auth/loggedin",
+      callback: "/auth/loggedin",
+      home: "/",
+      logout: "/",
     },
     strategies: {
       local: false,
@@ -125,14 +124,14 @@ module.exports = {
         domain: process.env.NUXT_ENV_AUTH_DOMAIN,
         clientId: process.env.NUXT_ENV_AUTH_CLIENT_ID,
         audience: process.env.NUXT_ENV_AUTH_AUDIENCE,
-        scope: ['openid', 'profile', 'email', 'offline_access'],
-        responseType: 'code',
-        grantType: 'authorization_code',
-        codeChallengeMethod: 'S256',
+        scope: ["openid", "profile", "email", "offline_access"],
+        responseType: "code",
+        grantType: "authorization_code",
+        codeChallengeMethod: "S256",
         logoutRedirectUri: process.env.NUXT_ENV_LOGOUT_REDIRECT_URL,
-      }
-    }
-  },  
+      },
+    },
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
@@ -142,7 +141,7 @@ module.exports = {
   axios: {
     // proxy: false,
     baseURL: process.env.NUXT_ENV_DOMAIN_HTTPS,
-    credentials: true
+    credentials: true,
   },
 
   publicRuntimeConfig: {
